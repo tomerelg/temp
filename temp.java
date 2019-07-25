e7e15fec5e28047f650cfae39ecd0003664f0878
Tomer Elgavish <telgav@gmail.com>
2019-07-26 00:54:55 +0300

    print hey

--- temp.java
+++ temp.java
@@ -1,7 +1,8 @@
 public class temp {
 
-public static void main(String[] args) {
-System.out.println("Hello World");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hey!");
+	}
 
 }
